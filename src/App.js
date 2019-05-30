9726cfa98e04f48b196879e1620e048fac82e746
Torbjörn <torbjorn.ekstedt@hotmail.com>
2019-05-30 12:42:47 +0200

    connected netlify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>Halloj!:)</h1>
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://larticlesapidotcom.000webhostapp.com/api/articles')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+          
+        })
+      });
+  }
+
+  render() {
+
+    var { isLoaded, items } = this.state;
+    console.log(items.data);
+
+    if(!isLoaded) {
+      return <div>Loading...</div>
+    }
+
+    else {
+      return (
+        <div className="App">
+            
+          <ul>
+            {items.data.map(item => (
+              <li key={item.id}>
+                Id: {item.id} | Completed: {item.title}
+              </li>
+            ))};
+          </ul>
+
+        </div>
+      );
+    }
+  }
+}
